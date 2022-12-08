bfc7c0dbcb70ce9615222ea3bd4c7ae426b43d33
linearcombination <4829djaskdfj@gmail.com>
2022-12-08 10:53:40 -0800

    More frontend UI tests
    Test two more important cases related to the settings screen.

--- frontend/tests/e2e/e2e.spec.ts
+++ frontend/tests/e2e/e2e.spec.ts
@@ -72,3 +72,70 @@ test('test pt-br, fr, mat, ulb, tn, tq, tw, sl-sr pdf', async ({ page }) => {
   await expect(page).toHaveURL('/#/result')
   await expect(page.locator('h3:has-text("Success!")')).toHaveText('Success!')
 })
+
+test('test ePub and Docx options not available when print layout is chosen, but PDF is available', async ({
+  page
+}) => {
+  await page.getByRole('button', { name: '1. Languages' }).click()
+  await expect(page).toHaveURL('/#/languages')
+  await page.getByLabel('Español (Latin American Spanish)').check()
+  await page.getByLabel('Brazilian Português').check()
+  await page.getByRole('button', { name: 'Add (2) Languages' }).click()
+  await expect(page).toHaveURL('/#/')
+  await page.getByRole('button', { name: '2. Books' }).click()
+  await expect(page).toHaveURL('/#/books')
+  await page.getByLabel('Genesis').check()
+  await page.getByRole('button', { name: 'Add (1) Books' }).click()
+  await expect(page).toHaveURL('/#/')
+  await page.getByRole('button', { name: '3. Resource types' }).click()
+  await expect(page).toHaveURL('/#/resource_types')
+  await page.getByLabel("Select all Brazilian Português's resource types").check()
+  await page
+    .getByLabel(
+      "Select all\n                  Español (Latin American Spanish)'s resource types"
+    )
+    .check()
+  await page.getByRole('button', { name: 'Add (8) Resource Types' }).click()
+  await expect(page).toHaveURL('/#/')
+  await page.getByRole('link', { name: 'Settings' }).click()
+  await expect(page).toHaveURL('/#/settings')
+  await expect(page.locator('span:has-text("Generate PDF")')).toBeVisible()
+  await expect(page.locator('span:has-text("Generate ePub")')).not.toBeVisible()
+  await expect(page.locator('span:has-text("Generate Docx")')).not.toBeVisible()
+})
+
+test('test assembly strategy drop down not available when print layout deselected and only one language chosen; PDF, ePub, Docx options present', async ({
+  page
+}) => {
+  await page.getByRole('button', { name: '1. Languages' }).click()
+  await expect(page).toHaveURL('/#/languages')
+  await page.getByLabel('Español (Latin American Spanish)').check()
+  await page.getByRole('button', { name: 'Add (1) Languages' }).click()
+  await expect(page).toHaveURL('/#/')
+  await page.getByRole('button', { name: '2. Books' }).click()
+  await expect(page).toHaveURL('/#/books')
+  await page.getByLabel('Genesis').check()
+  await page.getByRole('button', { name: 'Add (1) Books' }).click()
+  await expect(page).toHaveURL('/#/')
+  await page.getByRole('button', { name: '3. Resource types' }).click()
+  await expect(page).toHaveURL('/#/resource_types')
+  await page
+    .getByLabel(
+      "Select all\n                  Español (Latin American Spanish)'s resource types"
+    )
+    .check()
+  await page.getByRole('button', { name: 'Add (4) Resource Types' }).click()
+  await expect(page).toHaveURL('/#/')
+  await page.getByRole('link', { name: 'Settings' }).click()
+  await expect(page).toHaveURL('/#/settings')
+  await page
+    .locator(
+      'li:has-text("Print Optimization Enabling this option will remove extra whitespace") div'
+    )
+    .nth(3)
+    .click()
+  await expect(page.locator('select[name="assemblyStrategy"]')).not.toBeVisible()
+  await expect(page.locator('span:has-text("Generate PDF")')).toBeVisible()
+  await expect(page.locator('span:has-text("Generate ePub")')).toBeVisible()
+  await expect(page.locator('span:has-text("Generate Docx")')).toBeVisible()
+})

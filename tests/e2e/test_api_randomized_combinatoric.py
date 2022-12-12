17a3e8af7915851c4890191a612cd961cb0e04c8
linearcombination <4829djaskdfj@gmail.com>
2022-12-12 13:47:56 -0800

    Add module docstring

--- tests/e2e/test_api_randomized_combinatoric.py
+++ tests/e2e/test_api_randomized_combinatoric.py
@@ -1,3 +1,22 @@
+"""
+Useful to unearth combinations that (possibly) do not work (because
+no test has been run on them until now). Such failing tests can then
+be added as permanent non-random tests so that regressions are
+avoided.
+
+Since randomization is involved with respect to fixture combinations,
+it could generate invalid document requests instances that fail their
+model validation or may access resources which translators have not
+yet prepared to be in the required formats properly which will then
+cause the whole test suite to fail (depending on pytest config).
+Therefore these tests are marked with the custom pytest marker
+'randomized' to run them separately from the other tests. Note that
+tests exist in additional places that could overlap with these so you
+aren't not testing these at all by skipping this in the main test run.
+This is just additional coverage that could be run once in a while
+using the 'randomized' pytest marker.
+"""
+
 import pytest
 import requests
 from document.config import settings
@@ -5,15 +24,6 @@ from document.entrypoints.app import app
 from fastapi.testclient import TestClient
 
 from document.domain import model
-
-###################################
-## Randomized combinatoric tests ##
-###################################
-
-# Useful to unearth combinations that (possibly) do not work (because
-# no test has been run on them until now). Such failing tests can then
-# be added as permanent non-random tests so that regressions are
-# avoided.
 
 
 @pytest.mark.randomized

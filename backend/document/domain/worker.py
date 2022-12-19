c54bedf99f20c11c55da38c3c731b3a979254376
linearcombination <4829djaskdfj@gmail.com>
2022-12-19 10:34:37 -0800

    Simplify celery config

--- backend/document/domain/worker.py
+++ backend/document/domain/worker.py
@@ -2,8 +2,4 @@ import os
 from celery import Celery
 
 app = Celery(__name__)
-app.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
-app.conf.result_backend = os.environ.get(
-    "CELERY_RESULT_BACKEND", "redis://localhost:6379/0"
-)
-app.conf.imports = ("document.domain.document_generator",)
+app.config_from_object("document.domain.celeryconfig")

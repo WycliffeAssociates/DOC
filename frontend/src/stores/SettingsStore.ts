09173cb07d245ae91d56929bf9ebfb9df61c57e0
linearcombination <4829djaskdfj@gmail.com>
2023-01-10 20:42:54 -0800

    Better naming and stronger types

--- frontend/src/stores/SettingsStore.ts
+++ frontend/src/stores/SettingsStore.ts
@@ -1,9 +1,10 @@
 import { writable, Writable } from 'svelte/store'
 
-const languageBookOrder: string = <string>import.meta.env.VITE_LANGUAGE_BOOK_ORDER
+let groupingOrderDefault: string = <string>import.meta.env.VITE_LANGUAGE_BOOK_ORDER
 
-export let layoutForPrintStore: Writable<boolean> = writable(true)
-export let assemblyStrategyKindStore: Writable<string> = writable(languageBookOrder)
+export let layoutForPrintStore: Writable<boolean> = writable<boolean>(true)
+export let assemblyStrategyKindStore: Writable<string> =
+  writable<string>(groupingOrderDefault)
 export let generatePdfStore: Writable<boolean> = writable<boolean>(false)
 export let generateEpubStore: Writable<boolean> = writable<boolean>(false)
 export let generateDocxStore: Writable<boolean> = writable<boolean>(false)

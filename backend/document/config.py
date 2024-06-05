1c7268be926275682ddbef872c1240e51894764a
linearcombination <4829djaskdfj@gmail.com>
2024-06-04 21:16:52 -0700

    Handle new resource type tn-condensed
    Unlike TN resource, TN-condensed resource does not provide chapter
    intros so we have to make chapter_intro function slightly smarter.
    
    Added tests for PDF and Docx cases of tn-condensed use.

--- backend/document/config.py
+++ backend/document/config.py
@@ -116,7 +116,7 @@ class Settings(BaseSettings):
         *EN_USFM_RESOURCE_TYPES,
     ]
     TN_RESOURCE_TYPES: Sequence[str] = ["tn"]
-    EN_TN_RESOURCE_TYPES: Sequence[str] = ["tn-wa"]
+    EN_TN_RESOURCE_TYPES: Sequence[str] = ["tn-wa", "tn-condensed"]
     ALL_TN_RESOURCE_TYPES: Sequence[str] = [*EN_TN_RESOURCE_TYPES, *TN_RESOURCE_TYPES]
     TQ_RESOURCE_TYPES: Sequence[str] = ["tq"]
     EN_TQ_RESOURCE_TYPES: Sequence[str] = ["tq-wa"]
@@ -291,6 +291,7 @@ class Settings(BaseSettings):
         "ulb-wa": "https://content.bibletranslationtools.org/WycliffeAssociates/en_ulb",
         "udb-wa": "https://content.bibletranslationtools.org/WycliffeAssociates/en_udb",
         "tn-wa": "https://content.bibletranslationtools.org/WycliffeAssociates/en_tn",
+        "tn-condensed": "https://content.bibletranslationtools.org/WycliffeAssociates/en_tn_condensed",
         "tw-wa": "https://content.bibletranslationtools.org/WycliffeAssociates/en_tw",
         "tq-wa": "https://content.bibletranslationtools.org/WycliffeAssociates/en_tq",
         "bc-wa": "https://content.bibletranslationtools.org/WycliffeAssociates/en_bc",
@@ -306,6 +307,7 @@ class Settings(BaseSettings):
         "ulb-wa": "Unlocked Literal Bible (ULB)",
         # "udb-wa": "Unlocked Dynamic Bible (UDB)",
         "tn-wa": "ULB Translation Notes",
+        "tn-condensed": "ULB Condensed Translation Notes",
         "tq-wa": "ULB Translation Questions",
         "tw-wa": "ULB Translation Words",
         "bc-wa": "Bible Commentary",

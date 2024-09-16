f7479242cf16936d63d8de747b99f0e91fa8e7d9
linearcombination <4829djaskdfj@gmail.com>
2024-09-16 14:24:02 -0700

    Set test timeout much longer to help github actions server Frontend tests pass locally and they pass using 'act' which runs github actions locally, but they failed randomly on github action servers. So let's see if a longer timeout helps github actions server.

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -3,6 +3,7 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
     testDir: 'tests',
     testMatch: '**/*.ts'
+    timeout: 120000 // Set global timeout to 2 minutes
 }
 
 export default config

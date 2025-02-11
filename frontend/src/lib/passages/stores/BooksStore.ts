90858628692891708ed8932be1d2f121dae0db2c
linearcombination <4829djaskdfj@gmail.com>
2025-02-11 13:07:11 -0800

    Remove unused store

--- frontend/src/lib/passages/stores/BooksStore.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store';
-import type { Writable } from 'svelte/store';
-
-export let otBookStore: Writable<Array<string>> = writable<Array<string>>([]);
-export let ntBookStore: Writable<Array<string>> = writable<Array<string>>([]);
-export let bookCountStore: Writable<number> = writable<number>(0);

51cb11951a4e5e8ef8e4977a297a4f6622b0cff8
linearcombination <4829djaskdfj@gmail.com>
2025-02-10 19:42:30 -0800

    Update the placeholder text for the verse field

--- frontend/src/routes/passages/passages/BibleReferenceSelector.svelte
+++ frontend/src/routes/passages/passages/BibleReferenceSelector.svelte
@@ -53,7 +53,7 @@
     <input
       id="verses"
       type="text"
-      placeholder="e.g., 1,2,5-7"
+      placeholder="e.g., 1,2,5-7,20"
       class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
       on:input={handleVerseInput}
       bind:value={verseReference}

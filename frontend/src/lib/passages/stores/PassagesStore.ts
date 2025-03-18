b24bf7ba5b52df016e9529bc86f99fadf99dd210
linearcombination <4829djaskdfj@gmail.com>
2025-03-18 14:12:02 -0700

    Only add passage if not already in store
    This is important because in the passages app, the user might select
    to add NT Survey Reviewer's Guide passages en masse more than once in
    the UI and we would want the passages to be duplicated

--- frontend/src/lib/passages/stores/PassagesStore.ts
+++ frontend/src/lib/passages/stores/PassagesStore.ts
@@ -4,7 +4,6 @@ import type { PassageReferenceDto } from '../models/passage'
 // Define the passages store
 export const passagesStore = writable<PassageReferenceDto[]>([])
 
-// Define the addPassage method
 export const addPassageReference = (
   langCode: string,
   bookCode: string,
@@ -12,15 +11,30 @@ export const addPassageReference = (
   chapterNum: number,
   verseReference: string
 ) => {
-  passagesStore.update((currentPassages) => [
-    ...currentPassages,
-    {
-      id: Math.max(0, ...currentPassages.map((p) => p.id)) + 1,
-      langCode,
-      bookCode,
-      bookName,
-      chapterNum,
-      verseReference
+  passagesStore.update((currentPassages) => {
+    // Check if the passage already exists
+    const exists = currentPassages.some(
+      (p) =>
+        p.langCode === langCode &&
+        p.bookCode === bookCode &&
+        p.chapterNum === chapterNum &&
+        p.verseReference === verseReference
+    )
+
+    if (exists) {
+      return currentPassages // Return unchanged if passage exists
     }
-  ])
+
+    return [
+      ...currentPassages,
+      {
+        id: Math.max(0, ...currentPassages.map((p) => p.id)) + 1,
+        langCode,
+        bookCode,
+        bookName,
+        chapterNum,
+        verseReference
+      }
+    ]
+  })
 }

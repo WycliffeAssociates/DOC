f20b1baf8a72db1aaa3f29fc8898c1942575f2d4
linearcombination <4829djaskdfj@gmail.com>
2025-06-14 09:31:56 -0700

    Removal of commented out code

--- frontend/src/routes/stet/settings/+page.svelte
+++ frontend/src/routes/stet/settings/+page.svelte
@@ -1,73 +1,18 @@
 <script lang="ts">
-  // import type { SelectElement } from './types'
-  // import Switch from './Switch.svelte'
   import WizardBreadcrumb from '$lib/stet/WizardBreadcrumb.svelte'
   import WizardBasket from '$lib/stet/WizardBasket.svelte'
   import WizardBasketModal from '$lib/WizardBasketModal.svelte'
   import {
-    // layoutForPrintStore,
-    // assemblyStrategyKindStore,
-    // assemblyStrategyChunkSizeStore,
-    // docTypeStore,
     emailStore,
-    // limitTwStore,
     documentRequestKeyStore,
     settingsUpdated
-    // twResourceRequestedStore
   } from '$lib/stet/stores/SettingsStore'
   import { documentReadyStore, errorStore } from '$lib/stet/stores/NotificationStore'
-  // import {
-  //   resourceTypesStore,
-  //   resourceTypesCountStore,
-  //   usfmAvailableStore
-  // } from '$lib/stores/ResourceTypesStore'
-  // import { bookCountStore } from '$lib/stores/BooksStore'
   import { langCountStore } from '$lib/stet/stores/LanguagesStore'
   import GenerateDocument from './GenerateDocument.svelte'
   import LogRocket from 'logrocket'
   import CheckIcon from '$lib/CheckIcon.svelte'
 
-  // let chapter: SelectElement = {
-  //   id: 'chapter',
-  //   label: <string>import.meta.env.VITE_CHUNK_SIZE_CHAPTER
-  // }
-  // Set default value of chapter
-  // $assemblyStrategyChunkSizeStore = chapter.id
-
-  // Set whether TW has been requested for any of the languages
-  // requested so that we can use this fact in the UI to trigger the
-  // presence or absence of the toggle to limit TW words.
-  // let twRegexp = new RegExp('.*tw.*')
-  // $: {
-  //   if ($resourceTypesStore) {
-  //     $twResourceRequestedStore = $resourceTypesStore.some((item) => twRegexp.test(item))
-  //   }
-  // }
-  // $: {
-  //   if ($twResourceRequestedStore && $usfmAvailableStore) {
-  //     $limitTwStore = true
-  //   } else {
-  //     $limitTwStore = false
-  //   }
-  // }
-
-  // The 3rd party HTML to PDF conversion library we use, weasyprint,
-  // doesn't seem to be able to handle line length for the Khmer language
-  // which results in words overlapping each other when two column
-  // layout of Khmer content is displayed. Only TN and TQ resource types
-  // use two column layout and thus if those are selected by the user
-  // then the UI will exclude PDF as an output format choice for
-  // Khmer.
-  // let kmRegexp = new RegExp('km, tn, .*|km, tq, .*')
-  // let showPdfAsOption: boolean = true
-  // // $: console.log(`showPdfAsOption: ${showPdfAsOption}`)
-  // $: {
-  //   if ($resourceTypesStore) {
-  //     if ($resourceTypesStore.some((item) => kmRegexp.test(item))) {
-  //       showPdfAsOption = false
-  //     }
-  //   }
-  // }
 
   $: showEmail = false
   $: showEmailCaptured = false
@@ -120,118 +65,6 @@
     </div>
     <!-- main content -->
     <main class="flex-1 overflow-y-auto p-4">
-      <!-- <h3 class="mb-2 mt-2 text-2xl text-[#33445C]">File type</h3> -->
-      <!-- <div class="ml-4"> -->
-      <!--   <div class="mb-2"> -->
-      <!--     <label> -->
-      <!--       <input -->
-      <!--         name="docType" -->
-      <!--         value={'docx'} -->
-      <!--         bind:group={$docTypeStore} -->
-      <!--         type="radio" -->
-      <!--         on:change={() => { -->
-      <!--           $settingsUpdated = true -->
-      <!--           $errorStore = '' -->
-      <!--         }} -->
-      <!--         class="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600" -->
-      <!--       /> -->
-      <!--       <span class="text-xl text-[#33445C]">Docx</span> -->
-      <!--     </label> -->
-      <!--   </div> -->
-      <!--   <div class="mb-2"> -->
-      <!--     <label> -->
-      <!--       <input -->
-      <!--         name="docType" -->
-      <!--         value={'epub'} -->
-      <!--         bind:group={$docTypeStore} -->
-      <!--         type="radio" -->
-      <!--         on:change={() => { -->
-      <!--           $settingsUpdated = true -->
-      <!--           $errorStore = '' -->
-      <!--         }} -->
-      <!--         class="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600" -->
-      <!--       /> -->
-      <!--       <span class="text-xl text-[#33445C]">ePub</span> -->
-      <!--     </label> -->
-      <!--   </div> -->
-      <!--   {#if showPdfAsOption} -->
-      <!--     <div class="mb-2"> -->
-      <!--       <label> -->
-      <!--         <input -->
-      <!--           name="docType" -->
-      <!--           value={'pdf'} -->
-      <!--           bind:group={$docTypeStore} -->
-      <!--           type="radio" -->
-      <!--           on:change={() => { -->
-      <!--             $settingsUpdated = true -->
-      <!--             $errorStore = '' -->
-      <!--           }} -->
-      <!--           class="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600" -->
-      <!--         /> -->
-      <!--         <span class="text-xl text-[#33445C]">PDF</span> -->
-      <!--       </label> -->
-      <!--     </div> -->
-      <!--   {/if} -->
-      <!-- </div> -->
-      <!-- <h3 class="mb-2 mt-4 text-2xl text-[#33445C]">Layout</h3> -->
-      <!-- <div class="ml-4"> -->
-      <!--   {#if $langCodesStore[1]} -->
-      <!--     <div class="mb-2"> -->
-      <!--       <label> -->
-      <!--         <input -->
-      <!--           name="assemblyType" -->
-      <!--           value={'lbo'} -->
-      <!--           bind:group={$assemblyStrategyKindStore} -->
-      <!--           type="radio" -->
-      <!--           on:change={() => { -->
-      <!--             $settingsUpdated = true -->
-      <!--             $errorStore = '' -->
-      <!--           }} -->
-      <!--           class="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600" -->
-      <!--         /> -->
-      <!--         <span class="text-xl text-[#33445C]">Interleave content by book</span> -->
-      <!--       </label> -->
-      <!--     </div> -->
-      <!--     <div class="mb-6"> -->
-      <!--       <label> -->
-      <!--         <input -->
-      <!--           name="assemblyType" -->
-      <!--           value={'blo'} -->
-      <!--           bind:group={$assemblyStrategyKindStore} -->
-      <!--           type="radio" -->
-      <!--           on:change={() => { -->
-      <!--             $settingsUpdated = true -->
-      <!--             $errorStore = '' -->
-      <!--           }} -->
-      <!--           class="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600" -->
-      <!--         /> -->
-      <!--         <span class="text-xl text-[#33445C]">Interleave content by chapter</span> -->
-      <!--       </label> -->
-      <!--     </div> -->
-      <!--   {/if} -->
-      <!--   <div class="flex"> -->
-      <!--     <Switch bind:checked={$layoutForPrintStore} id="layout-for-print-store" /> -->
-      <!--     <span class="ml-2 text-xl text-[#33445C]">Print optimization</span> -->
-      <!--   </div> -->
-      <!--   <div class="mt-2"> -->
-      <!--     <span class="text-lg text-[#33445C]" -->
-      <!--       >Enabling this option will remove extra whitespace</span -->
-      <!--     > -->
-      <!--   </div> -->
-      <!--   {#if $twResourceRequestedStore && $usfmAvailableStore} -->
-      <!--     <div class="mb-2 mt-6 flex"> -->
-      <!--       <Switch bind:checked={$limitTwStore} id="limit-tw-store" /> -->
-      <!--       <span class="ml-2 text-xl text-[#33445C]">Limit TW words</span> -->
-      <!--     </div> -->
-      <!--     <div> -->
-      <!--       <span class="text-lg text-[#33445C]" -->
-      <!--         >Enabling this option will filter TW words down to only those that occur in the -->
-      <!--         scripture for the books chosen</span -->
-      <!--       > -->
-      <!--     </div> -->
-      <!--   {/if} -->
-      <!-- </div> -->
-
       <h3 class="mb-2 mt-4 text-2xl text-[#33445C]">Notification</h3>
       <div class="ml-4">
         {#if !$documentReadyStore}

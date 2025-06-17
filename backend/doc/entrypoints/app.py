73ac0139a5946c77c97c5f1a5cf4926e16561791
linearcombination <4829djaskdfj@gmail.com>
2025-06-16 18:57:04 -0700

    Remove obselete constant

--- backend/doc/entrypoints/app.py
+++ backend/doc/entrypoints/app.py
@@ -16,7 +16,6 @@ from fastapi.responses import JSONResponse
 
 # Docker container paths
 DOCKER_BASE_DIR = "/app"
-DOCKER_ASSETS_DOWNLOAD_DIR = join(DOCKER_BASE_DIR, settings.RESOURCE_ASSETS_DIR)
 DOCKER_EN_RG_DIR = join(DOCKER_BASE_DIR, settings.EN_RG_DIR)
 DOCKER_DOCX_FILE_SRC = join(DOCKER_BASE_DIR, "en_rg_nt_survey.docx")
 DOCKER_DOCX_FILE_DEST = join(DOCKER_EN_RG_DIR, "en_rg_nt_survey.docx")

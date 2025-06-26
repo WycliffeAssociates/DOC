f155a679c3d4a3e172929f4e00e8aee308508f57
linearcombination <4829djaskdfj@gmail.com>
2025-06-26 09:59:39 -0700

    Refactor css to resolve (most) svelte css compiler warnings

--- frontend/src/routes/passages/passages/+page.svelte
+++ frontend/src/routes/passages/passages/+page.svelte
@@ -125,31 +125,3 @@
     <WizardBasket />
   </div>
 </div>
-
-<style global lang="postcss">
-  #filter-gl-langs,
-  #filter-non-gl-langs {
-    text-indent: 17px;
-    padding-left: 5px;
-    margin-right: 5px;
-    background-image: url('data:image/svg+xml,<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.5014 14.0014H14.7114L14.4314 13.7314C15.0564 13.0054 15.5131 12.1502 15.769 11.2271C16.0248 10.3039 16.0735 9.33559 15.9114 8.39144C15.4414 5.61144 13.1214 3.39144 10.3214 3.05144C9.33706 2.92691 8.33723 3.02921 7.39846 3.35053C6.4597 3.67185 5.60688 4.20366 4.90527 4.90527C4.20366 5.60688 3.67185 6.4597 3.35053 7.39846C3.02921 8.33723 2.92691 9.33706 3.05144 10.3214C3.39144 13.1214 5.61144 15.4414 8.39144 15.9114C9.33559 16.0735 10.3039 16.0248 11.2271 15.769C12.1502 15.5131 13.0054 15.0564 13.7314 14.4314L14.0014 14.7114V15.5014L18.2514 19.7514C18.6614 20.1614 19.3314 20.1614 19.7414 19.7514C20.1514 19.3414 20.1514 18.6714 19.7414 18.2614L15.5014 14.0014ZM9.50144 14.0014C7.01144 14.0014 5.00144 11.9914 5.00144 9.50144C5.00144 7.01144 7.01144 5.00144 9.50144 5.00144C11.9914 5.00144 14.0014 7.01144 14.0014 9.50144C14.0014 11.9914 11.9914 14.0014 9.50144 14.0014Z" fill="%2366768B"/></svg>');
-    background-repeat: no-repeat;
-    background-position: left center;
-    outline: 0;
-  }
-  div.target:has(input[type='checkbox']:checked) {
-    background: #e6eefb;
-  }
-  div.target:has(input[type='radio']:checked) {
-    background: #e6eefb;
-  }
-  div.radio-target:has(input[type='radio']:checked) {
-    background: #e6eefb;
-  }
-  .radio-style {
-    @apply h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600;
-  }
-  .search-style {
-    @apply h-full w-full rounded-[7px] border border-gray-200 bg-transparent px-3 py-2.5 font-sans text-xl  font-normal text-[#33445c] outline outline-0 transition-all focus:border-2 focus:border-gray-900 focus:outline-0;
-  }
-</style>
--- frontend/src/routes/stet/settings/+page.svelte
+++ frontend/src/routes/stet/settings/+page.svelte
@@ -126,15 +126,3 @@
   </div>
   <!-- end if -->
 </div>
-
-<style global lang="postcss">
-  li.target:has(input[type='checkbox']:checked) {
-    background: #e6eefb;
-  }
-  input.checkbox-target[type='checkbox']:checked + span {
-    color: #015ad9;
-  }
-  .checkbox-style {
-    @apply h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600;
-  }
-</style>

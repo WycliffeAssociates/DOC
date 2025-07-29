76f8cc7236515f5229ff7dfaf712071439003725
linearcombination <4829djaskdfj@gmail.com>
2025-07-29 13:30:10 -0700

    Update doctests so that they all pass again

--- backend/doc/utils/list_utils.py
+++ backend/doc/utils/list_utils.py
@@ -11,3 +11,17 @@ def unique_tuples(lst: list[T]) -> list[T]:
     >>> assert unique_tuples(data_3) == [('x', 'y', True), ('x', 'y', False), ('a', 'b', True)]
     """
     return list(dict.fromkeys(lst))
+
+
+def unique_book_codes(lst: list[T]) -> list[T]:
+    """
+    >>> input_list = [("lev", "value1"), ("lev", "value2", True), ("abc", "value3"), ("abc", "value4", False)]
+    >>> result = unique_tuples(input_list)
+    [('lev', 'value1'), ('abc', 'value3')]
+    """
+    seen = {}
+    for item in lst:
+        key = item[0]
+        if key not in seen:
+            seen[key] = item
+    return list(seen.values())

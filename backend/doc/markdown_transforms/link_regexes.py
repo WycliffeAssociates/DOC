392e2dfb4f053815399196d8d5fcb89b6b37c9bb
linearcombination <4829djaskdfj@gmail.com>
2026-07-10 11:26:42 -0700

    Filter out (See: ...) forms in TW, TN, and (in the future maybe, TA)
    By request of PO

--- backend/doc/markdown_transforms/link_regexes.py
+++ backend/doc/markdown_transforms/link_regexes.py
@@ -2,7 +2,6 @@
 
 import re
 
-
 # Handle TW wikilink inner text
 TW_RC_LINK_RE = re.compile(
     (
@@ -184,4 +183,9 @@ TN_OBS_MARKDOWN_LINK_RE = re.compile(
 # More particularly:  [Covenant with David](../articles/covenantdavid.md)
 BC_MARKDOWN_LINK_RE = re.compile(
     r"\[(?P<link_text>.+?)\] *\(\.\.\/(?P<link_ref>articles.+?)\)"
+)
+
+SEE_PARENTHETICAL_RE = re.compile(
+    r"\(\s*See:\s*.*?\)",
+    re.IGNORECASE,
 )

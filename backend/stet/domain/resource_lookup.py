48c349f1d931491684373190f6c644ec286da322
linearcombination <4829djaskdfj@gmail.com>
2026-09-14 14:57:29 -0700

    Add bg and id languages to STET By request of PO

--- backend/stet/domain/resource_lookup.py
+++ backend/stet/domain/resource_lookup.py
@@ -1,7 +1,7 @@
 from typing import Sequence
 
 from doc.config import settings
-from doc.domain.resource_lookup import fetch_source_data
+from doc.domain.resource_lookup import fetch_source_data, fetch_non_primary_source_data
 from doc.utils.list_utils import unique_tuples
 
 # List of languages which do not have USFM available for NT books. We use this
@@ -30,6 +30,7 @@ def lang_codes_and_names_having_usfm(
     'aao'
     """
     data = fetch_source_data()
+    non_primary_data = fetch_non_primary_source_data()
     values = []
     if data is None or not data.git_repo:
         logger.info("Data API is down or no git_repo found!")
@@ -48,6 +49,40 @@ def lang_codes_and_names_having_usfm(
                 else:
                     values.append(
                         (ietf_code, f"{localized_name} ({english_name})", is_gateway)
+                    )
+    except Exception:
+        logger.exception("Failed due to the following exception.")
+    if non_primary_data is None or not non_primary_data.git_repo:
+        logger.info("Data API is down or no git_repo found!")
+        return []
+    try:
+        for non_primary_repo_info in non_primary_data.git_repo:
+            non_primary_language_info = non_primary_repo_info.content
+            non_primary_language = non_primary_language_info.language
+            non_primary_ietf_code = non_primary_language.ietf_code
+            non_primary_english_name = (
+                non_primary_language.english_name
+                if non_primary_language.english_name
+                else ""
+            )
+            non_primary_localized_name = non_primary_language.national_name
+            non_primary_is_gateway = non_primary_ietf_code in gateway_languages
+            if non_primary_ietf_code not in lang_code_filter_list:
+                if non_primary_english_name in non_primary_localized_name:
+                    values.append(
+                        (
+                            non_primary_ietf_code,
+                            non_primary_localized_name,
+                            non_primary_is_gateway,
+                        )
+                    )
+                else:
+                    values.append(
+                        (
+                            non_primary_ietf_code,
+                            f"{non_primary_localized_name} ({non_primary_english_name})",
+                            non_primary_is_gateway,
+                        )
                     )
     except Exception:
         logger.exception("Failed due to the following exception.")

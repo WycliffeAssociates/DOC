8173047f123cb61f9dc565f4d16302e9f59595ab
linearcombination <4829djaskdfj@gmail.com>
2026-09-21 14:36:28 -0700

    STET, Passages: Prefer certain USFM resources for certain languages

--- backend/doc/config.py
+++ backend/doc/config.py
@@ -20,7 +20,8 @@ class Settings(BaseSettings):
 
     DATA_API_URL: HttpUrl
 
-    LANGUAGES_WHERE_NON_ULB_PREFERRED: Sequence[str] = ["fr"]
+    PREFERRED_RESOURCES: Mapping[str, str] = {"fr": "f10",
+                                              "id": "tbi"}
 
     # This is only used to see if a lang_code is in the collection
     # otherwise it is a heart language. Eventually the graphql data api may
